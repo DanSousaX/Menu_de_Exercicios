b7cc5f22432e051b3c7e6d90f9a025284a9daa6a
Sousa <daniel._sousa@outlook.com>
2023-06-09 14:03:22 -0300

    Adição SwitchCase Exercício 1

--- MenuExercicio/Program.cs
+++ MenuExercicio/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,10 +23,30 @@ namespace MenuExercicio
             Console.WriteLine("\n 6) Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.");
             Console.WriteLine("\n 7) Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem \"Sao Multiplos\" ou \"Nao sao Multiplos\",\n indicando se os valores lidos são múltiplos entre si.\r\n Atenção: os números devem poder ser digitados em ordem crescente ou decrescente.");
             Console.WriteLine("\n 8) Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode\r\n começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas");
-
-
+            Console.Write("Número do Exercício: ");
+            exercicio = int.Parse(Console.ReadLine());
             //Codigo dos Exercicios
-
+            
+            switch (exercicio)
+                
+                {
+                case 1:
+                Console.Clear();
+                Console.WriteLine("\n 1) Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas,\n o valor que recebe por hora e calcula o salário desse funcionário.\n A seguir, mostre o número e o salário do funcionário, com duas casas decimais\n");
+                int id;
+                int horasTrabalhadas;
+                double vlrPorHora;
+                double salario;
+                Console.WriteLine("Informe a ID do funcionário: ");
+                id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Informe a quantidade de horas trabalhadas:");
+                horasTrabalhadas = int.Parse(Console.ReadLine());
+                Console.WriteLine("Informe o valor por hora trabalhada: ");
+                vlrPorHora = double.Parse(Console.ReadLine());
+                salario = horasTrabalhadas * vlrPorHora;
+                Console.WriteLine($"ID Funcionário: {id}");
+                break;
+            }
         }
     }
 }
